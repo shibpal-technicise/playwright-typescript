572b28d61b3c172a0562de552e0c691c4ffa2762
Shib Shankar Pal <shibpal@Shibs-MacBook-Air.local>
2026-05-29 00:16:50 +0530

    modify login_test to make data driven

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -6,12 +6,14 @@ export class LoginPage{
     readonly username: Locator;
     readonly password: Locator;
     readonly loginButton: Locator;
+    readonly errorMessage: Locator;
 
     constructor(page: Page){
         this.page = page;
         this.username = page.locator('#user-name');
         this.password = page.locator('#password');
         this.loginButton = page.locator('#login-button');
+        this.errorMessage = page.locator('[data-test="error"]');
     }
 
     async gotoLoginPage(){
@@ -24,7 +26,7 @@ export class LoginPage{
         await this.loginButton.click();
     }
 
-    async verifyLoginSuccess(){
-        await this.page.waitForURL('https://www.saucedemo.com/inventory.html');
-    }
+    // async verifyLoginSuccess(){
+    //     await this.page.waitForURL('https://www.saucedemo.com/inventory.html');
+    // }
 }
