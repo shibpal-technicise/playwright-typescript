e500709da88180ba252d0fc35c827df3bdd54afd
Shib Shankar Pal <shibpal@Mac-3.lan>
2026-05-28 17:19:14 +0530

    modify class

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -6,14 +6,12 @@ export class LoginPage{
     readonly username: Locator;
     readonly password: Locator;
     readonly loginButton: Locator;
-    //readonly errorMessage: Locator;
 
     constructor(page: Page){
         this.page = page;
         this.username = page.locator('#user-name');
         this.password = page.locator('#password');
         this.loginButton = page.locator('#login-button');
-        //this.errorMessage = page.locator('[data-test="error"]');
     }
 
     async gotoLoginPage(){
